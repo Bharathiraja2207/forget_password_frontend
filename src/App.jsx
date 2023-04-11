10b3cdd9940023d99b76fcb21e8a561342d851f0
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-04-11 18:41:59 +0530

    forget password

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import './App.css'
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -7,18 +9,8 @@ import { Sendotp, Verifyotp } from './Sendotp';
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 
 export default function App() {
-  
-  const handleClick = () => {
-    localStorage.removeItem('token');
-    setTimeout(() => {
-      navigate("/login")
-    }, 1500);
-    console.log("logout")
-  }
-
   return (
     <div className="App">
-      <Button onClick={handleClick} variant="contained">LOGOUT</Button>
 <Routes>
         <Route path="/" element={<Signin />} />
         <Route path="/login" element={<Login />} />
@@ -44,9 +36,17 @@ function Shorturl(){
   const navigate = useNavigate()
 
 
+  const handleClick = () => {
+    localStorage.removeItem('token');
+    setTimeout(() => {
+      navigate("/login")
+    }, 1500);
+    console.log("logout")
+  }
+
   return(
     <div className='parrent'>
-      
+      <Button onClick={handleClick} variant="contained">LOGOUT</Button>
       <Card sx={{ maxWidth: 500 }}>
       <CardContent>
       <img src='https://media.istockphoto.com/id/1392896428/photo/inspirational-quote.jpg?s=612x612&w=0&k=20&c=CbqPLlx65768zd6QQpJqo55MZIAhA_o68cS0nLIfjw0='></img>
