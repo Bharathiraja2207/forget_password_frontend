552e08ad6dbc8e4051cc63d72825904a31b9715e
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-04-11 18:33:56 +0530

    forget password frontend

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Signin, Login } from './Signin';
+import { Sendotp, Verifyotp } from './Sendotp';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+
+export default function App() {
+  
+  const handleClick = () => {
+    localStorage.removeItem('token');
+    setTimeout(() => {
+      navigate("/login")
+    }, 1500);
+    console.log("logout")
+  }
+
+  return (
+    <div className="App">
+      <Button onClick={handleClick} variant="contained">LOGOUT</Button>
+<Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forget-password" element={<Sendotp />} />
+        <Route path="/verify" element={<Verifyotp />} />
+        <Route path="/url-Shortening" element={<ProtectedRoute> <Shorturl/></ProtectedRoute>} />
+      </Routes>
+    </div>
+  )
+}
+
+
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem('token');
+  // const token=false;
+  return (
+    token ? <section>{children}</section> : <Navigate replace to="/" />
+    //  token? <section>{children}</section>:<h1>unautharaied</h1>
+  )
+}
+
+function Shorturl(){
+  const navigate = useNavigate()
+
+
+  return(
+    <div className='parrent'>
+      
+      <Card sx={{ maxWidth: 500 }}>
+      <CardContent>
+      <img src='https://media.istockphoto.com/id/1392896428/photo/inspirational-quote.jpg?s=612x612&w=0&k=20&c=CbqPLlx65768zd6QQpJqo55MZIAhA_o68cS0nLIfjw0='></img>
+    </CardContent>
+    </Card>
+  </div>
+  )
+}
+
+
+
+
